92e10019d40bb3a251f679cb48abdf1f52b6f46b
Nathan Le Roux <nathrun@iafrica.com>
2017-07-06 18:05:10 +0200

    intial testing and fixes to stepperController class

--- stepperModule.py
+++ stepperModule.py
@@ -51,49 +51,53 @@ class stepperController(object):
         #add some checks for the values entered
         if(type(self.pins) != list):
             #send exception
+            print('please enter list')
 
     #---end of def __init__-----------------------------------------------------
 
     #Function returns a bool, False if any arguments are not correct
     #and True once the stepper motor has completed spinning.
-    def spinMotor(numRevolution, stepPhase="dual", stepType='full'):    #<-- needs to be tested
-        if(stepPhase != 'dual' or stepPhase != 'single'):
-            return False
+    def spinMotor(self, numRevolution, stepPhase="dual", stepType='full'):    #<-- needs to be tested
+        if(stepPhase != 'dual' and stepPhase != 'single'):
+            return 'fail 1'
             #should change to throw exception as well for more detail
-        if(stepType != 'half' or stepType != 'full'):
-            return False
+        if(stepType != 'half' and stepType != 'full'):
+            return 'fail 2'
             #should change to throw exception as well for more detail
 
 
         curSeq = []
-        steps = stepsInRevolution
+        steps = self.stepsInRevolution
         if(stepPhase == 'single'):
             if(stepType == 'half'):
                 print('can not do half steps on single phase stepping. defualted to full steps')
                 #possibly change color to red to make more visible
-            curSeq = singlePhaseStepping
+            curSeq = self.singlePhaseStepping
         elif(stepType == 'half'):
-            curSeq = dualPhaseStepping[0]
+            curSeq = self.dualPhaseStepping[1]
             steps = steps*2
         else:
-            curSeq = dualPhaseStepping[1]
+            curSeq = self.dualPhaseStepping[0]
 
         #assign GPIO pins here
-        if(GPIO.getmode != GPIO.Board):
-            GPIO.setmode(GPIO.Board)
+        if(GPIO.getmode != GPIO.BCM):
+            GPIO.setmode(GPIO.BCM)
         for pin in self.pins:
-            GPIO.setup(pin, GPIO.OUT, initial=0)
+            GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
 
         #make for loop to run through curSeq to make motor spin the correct amount of times
         #do not forget to add sleep
         phase = 0
-        for x in range(round(steps*numRevolution)):
+        for x in range(int(round(steps*numRevolution))):
             for pin in range(4):
-                GPIO.output(pin, curSeq[phase][pin])
+                GPIO.output(self.pins[pin], curSeq[phase][pin])
             time.sleep(self.stepBreak)
             phase += 1
             if(phase >= len(curSeq)):
                 phase = 0
+
+        for pin in self.pins:
+            GPIO.output(pin,0)
 
         #end of turning phase
 
